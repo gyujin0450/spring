879e0f3b919dd875033ab7b1ed0a8ad1552bfdce
최규진 <gyujin0450@naver.com>
2019-12-07 11:43:00 +0900

    Model에 받오는 방법

--- day2/src/main/java/com/iu/s1/board/notice/NoticeController.java
+++ day2/src/main/java/com/iu/s1/board/notice/NoticeController.java
@@ -1,20 +1,27 @@
 package com.iu.s1.board.notice;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.iu.s1.board.BoardVO;
 
 @Controller
 @RequestMapping(value = "/notice/**") // Notice 아래 모든 폴덜/파일
 public class NoticeController {
 	
+	@Autowired
+	private NoticeService noticeService;
+	
 	@RequestMapping(value="boardList", method = {RequestMethod.POST, RequestMethod.GET })
-	public String boardList(NoticeVO noticeVO) throws Exception {
-		// TO DO : 멤버변수로 갖는 Bean(VO) 선언 
-		System.out.println(noticeVO.getNum());
-		System.out.println(noticeVO.getTitle());
-		System.out.println("notieBoard");
+	public String boardList() throws Exception {
+//		List<BoardVO> ar = noticeService.boardList();
+
 		return "board/boardList";
 	}
 	
@@ -29,13 +36,16 @@ public class NoticeController {
 	}
 	
 	@RequestMapping(value = "boardSelect")
-	public ModelAndView boardSelect() throws Exception {
-		ModelAndView mv = new ModelAndView();
-		System.out.println("boardSelect");
+	public String boardSelect(BoardVO boardVO, Model model) throws Exception {
+		boardVO = noticeService.boardSelect(boardVO);
 		
-		mv.setViewName("board/boardSelect");
+//		System.out.println(boardVO.getTitle());
+//		System.out.println(boardVO.getNum());
 		
-		return mv;
+		model.addAttribute("vo", boardVO);
+		
+		return "board/boardSelect";
+		
 	}
 	
 	
--- day2/src/main/webapp/WEB-INF/views/board/boardSelect.jsp
+++ day2/src/main/webapp/WEB-INF/views/board/boardSelect.jsp
@@ -1,13 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<% %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>    
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<c:import url="../templete/bootStrap.jsp"></c:import>
 </head>
 <body>
-	<h1>Board Select</h1>
+<c:import url="../templete/nav.jsp"></c:import>
+	<h1>Board Select Page...</h1>
+	<!-- EL -->
+	<h1>Num : ${vo.num}</h1>
+	<h1>Title : ${vo.title}</h1>
 </body>
 </html>
